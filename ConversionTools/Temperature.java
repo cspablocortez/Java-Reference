68d1d5783b3fb8dfefce2c4edbb81524dec487cc
Pablo Cortez <cspablocortez@gmail.com>
2023-11-28 18:10:13 -0800

    File hierarchy

---
+++ ConversionTools/Temperature.java
@@ -0,0 +1,15 @@
+package ConversionTools;
+
+public class Temperature {
+    // Select input file:
+       // -- celsius.in
+       // -- fahrenheit.in
+
+       System.out.println("Select input file: ");
+       // print a tree representation of the files inside a directory
+
+    // Temperatures converted and saved to Temperates.out
+
+
+
+}
