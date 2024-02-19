1d0ffc7b642554388be7eeb35845c2b8257f6e11
Pablo Cortez <cspablocortez@gmail.com>
2024-02-18 19:51:38 -0500

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,10 +1,16 @@
 public class Recursion {
-   public static void main(String[] args) {
-    int upTo = 4;
-    for (int i = 0; i <= upTo; i++) {
-        System.out.println(fibonacci(i));
+    public static void main(String[] args) {
+        int upTo = 4;
+        int[] fibArray = new int[fibonacci(upTo)];
+
+        for (int i = 0; i < upTo; i++) {
+            fibArray[i] = fibonacci(i);
+        }
+
+        for (int i = 0; i < upTo; i++) {
+            System.out.println(fibArray[i]);
+        }
     }
-   }
 
     private static int fibonacci(int n) {
         if (n == 1 || n == 0) {
@@ -12,5 +18,42 @@ public class Recursion {
         } else {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
-    } 
+    }
+}
+
+// -------------------------------------------------
+
+// A
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(sequence(i));
+        }
+    }
+    public static int sequence(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return sequence(n - 1) + sequence(n - 2);
+        }
+    }
+}
+
+import java.util.ArrayList; 
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Int> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(sequence[i])
+        }
+        System.out.println(list);
+    }
+    public static int sequence(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return sequence(n - 1) + sequence(n - 2);
+        }
+    }
 }
