dbe1dec354089b86525c2974a2b5a6013db14b63
Pablo Cortez <cspablocortez@gmail.com>
2024-03-03 20:54:36 -0500

    Refactor

--- CodingBat.java
+++ CodingBat.java
@@ -1,8 +1,23 @@
 public class CodingBat {
     public static void main(String[] args) {
-        int[] scores = {1, 3, 2}; // false
-        System.out.println(scoresIncreasing(scores));
+        int result = sumHeights2(new int[]{5, 3, 6, 7, 2}, 2, 4); // → 7
+        System.out.println(result);
     }
+
+
+    public static int sumHeights2(int[] heights, int start, int end) {
+        // Find differences first
+
+        int sum = 0;
+
+        for (int i = start; i < end; i++) {
+            int difference = Math.abs(heights[i] - heights[i + 1]);
+            System.out.println(difference);
+        }
+
+        return sum;
+    }
+    
 
 
     public static boolean scoresIncreasing(int[] scores) {
--- Pascal.java
+++ Pascal.java
@@ -1,5 +1,31 @@
 public class Pascal {
     public static void main(String[] args) {
+        int numRows = 7; 
         
+        StringBuilder output = new StringBuilder();
+        
+        int[][] triangle = generatePascalsTriangle(numRows);
+        
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j <= i; j++) {
+                output.append(triangle[i][j]).append(" ");
+            }
+            output.append("\n");
+        }
+
+        System.out.println(output.toString());
+    }
+
+    private static int[][] generatePascalsTriangle(int numRows) {
+        int[][] triangle = new int[numRows][numRows];
+        
+        for (int i = 0; i < numRows; i++) {
+            triangle[i][0] = 1;
+            for (int j = 1; j <= i; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+        
+        return triangle;
     }
 }
