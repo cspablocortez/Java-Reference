ec64ce0e236a2108cc79e3a03f6b1711cd99ec96
Pablo Cortez <cspablocortez@gmail.com>
2024-03-03 19:53:09 -0500

    Create CodingBat.java

---
+++ CodingBat.java
@@ -0,0 +1,16 @@
+public class CodingBat {
+    public static void main(String[] args) {
+        int[] scores = {1, 3, 2}; // false
+        System.out.println(scoresIncreasing(scores));
+    }
+
+
+    public static boolean scoresIncreasing(int[] scores) {
+        for (int i = 1; i < scores.length; i++) {
+          if (scores[i] < scores[i - 1]) {
+            return false;
+          }
+        }
+        return true;
+      }
+}
